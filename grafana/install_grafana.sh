8637fa68a58a9a894d17a7b9f0d0699700990eca
Navneet Toppo <53862587+nav-stack@users.noreply.github.com>
2022-09-01 15:58:47 +0530

    Add files via upload

---
+++ grafana/install_grafana.sh
@@ -0,0 +1,30 @@
+sudo su
+sudo tee /etc/yum.repos.d/grafana.repo<<EOF
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+sudo tee /etc/yum.repos.d/grafana.repo<<EOF
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/enterprise/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+sudo yum install -y grafana
+rpm -qi grafana
+
+sudo systemctl enable --now grafana-server
+systemctl status grafana-server.service
---
+++ node-exporter/install_node-exporter-ext.sh
@@ -0,0 +1,5 @@
+sudo su
+wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0-rc.0/node_exporter-1.4.0-rc.0.linux-amd64.tar.gz
+tar -xvf node_exporter-1.4.0-rc.0.linux-amd64.tar.gz
+rm -rf node_exporter-1.4.0-rc.0.linux-amd64.tar.gz
+./node_exporter-1.4.0-rc.0.linux-amd64/node_exporter 
